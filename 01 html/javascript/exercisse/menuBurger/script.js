58d66811539983aec5550865c3ce85b055eab191
lounis59 <lounis.habrouche@gmail.com>
2024-03-20 16:15:00 +0100

    exercisse menu burger1.0

--- 01 html/javascript/exercisse/menuBurger/script.js
+++ 01 html/javascript/exercisse/menuBurger/script.js
@@ -1,39 +1,44 @@
-"use strict"
+"use strict";
 
-const menu = document.querySelector('.menuDeroule');
-const bouton = document.querySelector('.bar');
-const bar1 = document.querySelector('.bar1');
-const bar2 = document.querySelector('.bar2');
-const bar3 = document.querySelector('.bar3');
+const menu = document.querySelector(".menuDeroule");
+const bouton = document.querySelector(".bar");
+const bar1 = document.querySelector(".bar1");
+const bar2 = document.querySelector(".bar2");
+const bar3 = document.querySelector(".bar3");
 
+bouton.addEventListener("click", () => {
+  if (menu.style.width == "") {
+    bar1.style.transition = "2s";
+    bar3.style.transition = "2s";
+    bar2.style.transition = "2s";
+    bar1.style.rotate = "45deg";
+    bar1.style.translate = "0 10px";
+    bar2.style.backgroundColor = "transparent";
+    bar3.style.rotate = "-45deg";
+    bar3.style.top = "50%";
+  } else {
+    menu.style.width = "";
+    bar1.style.rotate = "";
+    bar1.style.translate = "";
+    bar1.style.tranformeOrigine = "";
+    bar2.style.backgroundColor = "";
+    bar3.style.rotate = "";
+    setTimeout(()=>{
+        bar3.style.top = "";
+        bouton.style.backgroundColor = "";
 
-bouton.addEventListener('click',()=>{
-    if(menu.style.width == ""){
-        menu.style.width = "90vw"
-        menu.style.transition = "2s"
-        bar1.style.transition = "2s"
-        bar3.style.transition = "2s"
-        bar2.style.transition = "2s"
-        bar1.style.rotate = "45deg"
-        bar1.style.translate = "0 10px"
-        // bar1.style.tranformeOrigine = "-50%"
-        bar2.style.backgroundColor = "transparent"
-        bar3.style.rotate = "-45deg"
-        bar3.style.translate = "0 -4px"
-    }else{
-        menu.style.width = ""
-        bar1.style.rotate = ""
-        bar1.style.translate = ""
-        bar1.style.tranformeOrigine = ""
-        bar2.style.backgroundColor = ""
-        bar3.style.rotate = ""
-        bar3.style.translate = ""
-        
-    }
-    if(bar1.style.rotate === "45deg"){
-        bouton.style.rotate = "360deg"
-        bouton.style.transition = "1s"
-        
-    }
-    
-})
+    },500)
+  }
+  if (bar1.style.rotate === "45deg") {
+    setTimeout(() => {
+      bar1.style.tranformeOrigine = "-50%";
+      bar1.style.rotate = "405deg";
+      bar1.style.transition = "2s";
+      bar3.style.rotate = "315deg";
+      bar3.style.transition = "2s";
+      menu.style.width = "90vw";
+      menu.style.transition = "2s";
+      bouton.style.backgroundColor = "greenyellow";
+    }, 1000);
+  }
+});
