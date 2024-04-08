0d6f6fba5a7a06d74821a0c9f8bd334ac9393a0c
Lounis59 <you@example.com>
2024-04-08 16:27:31 +0200

    cours Angular

---
+++ 01 html/javascript/angular/mamietonne/src/app/app.component.ts
@@ -0,0 +1,36 @@
+import { Component,OnInit } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { RECETTES } from './RecetteList';
+import { Recette } from './Recette';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, CommonModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent implements OnInit {
+  title = 'mamietonne';
+  recetteList = RECETTES;
+  recetteSelected: Recette|undefined;
+
+  ngOnInit(): void {
+    console.table(this.recetteList);
+    // this.selectRecette(this.recetteList[0])
+  }
+  selectRecette(recetteId:string):void {
+    const index: number = parseInt(recetteId);
+    const recette: Recette|undefined = this.recetteList.find(rec=>rec.id === index)
+    if(recette)
+    {
+      console.log(`Vous avez sélectionné ${recette.name}`);
+      
+    }else{
+      console.log("Aucune recette corespondante");
+    }
+    this.recetteSelected = recette;
+    
+  }
+}
